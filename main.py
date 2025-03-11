c3606d6fb291b2d1c467e53da3f8e40fd8c305ca
Neel Agarwal <neelagarwal98@gmail.com>
2025-03-11 16:39:27 -0400

    completed main function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from load_data import data
 from text_processing import tokenize_text, tag_text, lemmatize_text, remove_stopwords, clean_text
 from count_vectorizer import return_count_embeddings
 from tfidf_vectorizer import return_tfidf_embeddings
-
+from modelling import return_classification_accuracy
+import pandas as pd
 
 def main():
     
@@ -14,9 +15,25 @@ def main():
     
     # adding the cleaned-text to the dataframe
     data['text_clean'] = cleaned_list_of_list
-   
-   
+    
+    indices = [(1,x) for x in range(1,4)]
+    columns = ["count","tfidf"]
 
+    accuracies_df = pd.DataFrame(columns=columns,index=pd.MultiIndex.from_tuples(indices)) # hierarchical indexing for tuple ngrams
+    
+    vectorizer_function_map = {
+        "count" : return_count_embeddings,
+        "tfidf" : return_tfidf_embeddings
+    }
+    
+    for vectorizer_type in ["count","tfidf"]:
+        for b in range(1,4): # the second part of the tuple for ngram range, the first part is always 1
+            ngram = (1,b)
+            X = vectorizer_function_map[vectorizer_type](data['text_clean'],ngram)
+            y = data['label']
+            
+            accuracies_df.loc[ngram,vectorizer_type] = return_classification_accuracy(X,y)
+    print(accuracies_df)
 
 if __name__ == "__main__":
     main()
